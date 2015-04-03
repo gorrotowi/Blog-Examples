b76cae77b3052f46d531714244ab9ab259525da0
gorrotowi <sebastian.tellez.o@gmail.com>
2015-04-02 19:28:14 -0600

    add data to list

--- ListAndWebservice/app/src/main/java/com/gorro/listandwebservice/MainActivity.java
+++ ListAndWebservice/app/src/main/java/com/gorro/listandwebservice/MainActivity.java
@@ -2,18 +2,76 @@ package com.gorro.listandwebservice;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
     String url = "http://api.citybik.es/v2/networks/ecobici";
+    ListView listView;
+
+    AdapterList adapter;
+    ArrayList<ItemList> data = new ArrayList<>();
+
+    RequestQueue rq;
+    JsonObjectRequest jsonRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listMain);
+        rq = Volley.newRequestQueue(MainActivity.this);
+
+        makePetition();
+
+    }
+
+    private void makePetition() {
+
+        jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonStations = response.getJSONObject("network").getJSONArray("stations");
+                    for (int i = 0; i < jsonStations.length(); i++) {
+                        String name = jsonStations.getJSONObject(i).getString("name");
+                        String address = jsonStations.getJSONObject(i).getJSONObject("extra").getString("address");
+                        data.add(new ItemList(name, address));
+                    }
+                    adapter = new AdapterList(MainActivity.this, data);
+                    listView.setAdapter(adapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //TODO
+            }
+        });
+
+        rq.add(jsonRequest);
+
     }
 
 
